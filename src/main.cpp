36b438e979dd69e7b2c1a93953dd9307f526a72e
Alon A. Rabinowitz <alonrab@outlook.com>
2024-12-30 20:00:53 +0200

    Add ESP32Servo library and implement servo test in main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
+
+#define SERVO_PIN 17
+
+Servo servo1;
 
 void setup()
 {
     Serial.begin(115200);
+    servo1.attach(SERVO_PIN);
 }
 
 void loop()
 {
+    for (int posDegrees = 0; posDegrees <= 180; posDegrees++)
+    {
+        servo1.write(posDegrees);
+        Serial.println(posDegrees);
+        delay(20);
+    }
+
+    for (int posDegrees = 180; posDegrees >= 0; posDegrees--)
+    {
+        servo1.write(posDegrees);
+        Serial.println(posDegrees);
+        delay(20);
+    }
+    // servo1.write(0);
 }
